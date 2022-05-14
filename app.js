285f56796be1ec4a815c4e9b346d7fe98c8c8d66
Shivam Modanwal <shivammodanwal6087@gmail.com>
2022-05-14 17:05:49 +0530

    adding better

--- app.js
+++ app.js
@@ -140,12 +140,9 @@ app.post("/delete", function(req, res){
 app.get("/about", function(req, res){
   res.render("about");
 });
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
 
 
-app.listen(port, function() {
+
+app.listen(process.env.PORT||3000, function() {
   console.log("Server started on port 3000");
 });
